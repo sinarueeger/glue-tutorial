6eceb3b079547dec4f1cc87a0adcb8a249bfef61
Sina Rueger <sina.rueeger@gmail.com>
2020-07-10 15:43:24 +0300

    adding Rmd file instead

--- glue-examples.R
+++ glue-examples.R
@@ -1,44 +1,81 @@
-## Teaching glue
-## --------------
+## Glue numbers and letters in R
+## Slides: https://sinarueeger.github.io/glue-tutorial/slides/slides#1
+## ////////////////////////////////////////////////////////////////////////////
 
 ## install.packages("glue")
-
 library(glue)
-library(dplyr) ## for data
+library(dplyr)
 
-apropos("glue")
+## glue two scalars ---------------------
 
-
-## glue two scalars
-## ---------------------
 animals <- "Elephants"
 number <- 3
 glue("There were {animals}")
+
 glue("There were {number} {animals}")
 
-## collapse vector
-## ---------------
-## > paste(,... )
-genes <- c("FTO", "PSCK3", "DTN3", "sadlfkjsdlfjsldfjlsdjf")
-glue_collapse(genes, sep = ", ", last = " and ")
+glue("There were {number} {animals}", number = 2, animals = "Giraffe")
 
-## same as paste(genes, collapse = ", ")
 
-## with pipe > df output
-## -----------------------
+## Exercise -----------------------------
+
+## Tweak the following R command so that it returns:
+## Yesterdays average temperature was 20.2 celsius.
+
+metric <- "celsius"
+temperature <- 20.2
+glue("Yesterdays average temperature was [metric] [temperature].")
+
+## collapse vector -----------------------
+
+genes <- c("FTO", "PSCK3", "DTN3", "NRT")
+glue_collapse(genes)
+glue_collapse(genes, sep = " + ")
+
+## Exercise ------------------------------
+
+## Predict the output of the following R code and choose solution 1, 2 or 3 below.
+
+trees <- c("pine", "fir", "cedar", "spruce", "hemlock")
+glue_collapse(trees, sep = ",", last = "and")
+
+# 1. `pine, fir, cedar, spruce and hemlock`
+# 2. `pine,fir,cedar,spruceandhemlock`
+# 3. `pine, fir, cedar, spruceandhemlock`
+
+
+## create new column with pipe -----------
+
 starwars %>% select(name, homeworld) %>% mutate(name_homeworld = glue("{name} ({homeworld})"))
-## same as 
-## starwars %>% select(name, homeworld) %>% mutate(hame_homeworld = paste(name, " (", homeworld, ")", sep = ""))
 
-## with pipe > vector output
-## -----------------------
+
+## create new vector with pipe -----------
+
 starwars %>% glue_data("{name} ({homeworld})", .na = "unknown")
-## same as paste(starwars$name, " (", starwars$homeworld, ")", sep = "")
 
-## store file
-## -----------------------
+## Exercise ------------------------------
+
+## Given the data below, create an extra column id, 
+## which is a combination of element and measure, 
+## separated with an underscore (e.g. "1_A")
+
+dat <- data.frame(element = c(1, 1, 2, 2), 
+                  measure = c("A", "B", "A", "B"))
+
+## store file ---------------------------
+
 dat <- starwars %>% select(name, homeworld) 
 dir <- "/Users/rueger/Projects/" 
 filename <- "table_1.txt"
 
 write_tsv(dat, path = glue("{dir}{filename}"))
+
+
+## find out more about glue ---------------
+
+apropos("glue")
+
+# - https://glue.tidyverse.org/index.html
+# - glue also appears in the `stringr` cheatsheet:
+#   https://raw.githubusercontent.com/rstudio/cheatsheets/master/strings.pdf
+
